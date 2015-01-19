d182af7ef60b57c7dd52a5e0b9247a08aae72061
CorentinHardy <corentin.fouyaya@hotmail.fr>
2015-01-19 13:17:25 +0100

    mise à jour de coffe

--- model/item/Coffee.java
+++ model/item/Coffee.java
@@ -1,16 +1,32 @@
 package model.item;
-public class Coffee extends Item {
-	private int energy;
 
-	public void drink() {
-		throw new UnsupportedOperationException();
-	}
+/**
+ * Describe a coffe, effect() restore energy.
+ * 
+ * @author Corentin Hardy
+ *
+ */
+public class Coffe extends Item {
+    private final int normalCoffe = 10;
+    private final int moreEnergy;
 
-	public int getEnergy() {
-		return this.energy;
-	}
+    public Coffe(Player player) {
+        super(player, "normal coffe", "a delicious coffe");
+        moreEnergy = normalCoffe;
+    }
 
-	public void setEnergy(int energy) {
-		this.energy = energy;
-	}
+    @Override
+    public void effect() {
+        if (!getPlayer().haveThisItem(this))
+            System.err.println(Language.translate("Cannot") + " "
+                    + Language.translate("use") + " "
+                    + Language.translate("this") + ": '" + this + "'"); // XXX
+        else {
+            System.out.println(Language.translate("You drunk your") + " "
+                    + Language.translate(getName()) + ", "
+                    + Language.translate("and regain some energy."));
+            game.upgradeEnergyStudent(moreEnergy);
+            getPlayer().removeItem(this);
+        }
+    }
 }
