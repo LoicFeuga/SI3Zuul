673412d6282b21bcbaeb6b41b0e8e33125a4c9c5
CorentinHardy <corentin.fouyaya@hotmail.fr>
2015-01-19 13:15:56 +0100

    mise a jour d'item

--- model/item/Item.java
+++ model/item/Item.java
@@ -1,8 +1,52 @@
 package model.item;
 
+import zuul.Player;
+import zuul.items.Item;
 import model.room.Room;
 
-public class Item {
-	public Room createur;
-	
+public abstract class Item {
+    private Game game;
+    private String name;
+    private String description;
+
+    public Item(Game game, String name, String description) {
+        this.game = game;
+        this.name = name;
+        this.description = description;
+    }
+    
+    // getters
+    public Game getGame() {
+        return player;
+    }
+    public String getName() {
+        return name;
+    }
+    public String getDescription() {
+        return description;
+    }
+
+    // setters
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    // others methods
+    abstract public void effect();
+
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (!(obj instanceof Item)) {
+            return false;
+        }
+        Item other = (Item) obj;
+        return name.equals(other.getName())
+                && description.equals(other.getDescription())
+                && player.equals(other.getPlayer());
+    }
 }
